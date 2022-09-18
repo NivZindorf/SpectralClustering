4a43997c703d17ec06e6e1d434167a404e6ee718
NivZindorf <100680727+NivZindorf@users.noreply.github.com>
2022-09-18 09:42:00 +0300

    Update spkmeans.h

--- src/spkmeans.h
+++ src/spkmeans.h
@@ -1,14 +1,3 @@
-/*typedef struct {
-double ** centroid0;
-double ** centroid1;
-int * counters;
-int prev; 
-}Centroids;
-void find_centroids(Centroids * centroids,double ** data,int k, int dimensional,int vec_counter);
-Centroids* create_centroids(double ** data, double ** cent, int k, int dimensional);
-void compute (Centroids * centroids,double ** data, int k, int dimensional,int max_iter,double EPS,int vec_counter);
-double distance(double * x,double * y, int dimensional);
-int deltamiu(Centroids * centroids,int k , int dimensional,double EPS);*/
 int fit(double** data, double** cent,int k,int dim,int vec_count);
 void to_weighted(double ** weight, double ** vectors,int dim,int vec_counter);
 void to_diagonal(double ** diagoanl,double ** weight,int vec_counter);
